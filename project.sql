aaa0fc312061e230aa8c3e3c02af8ecf3e8301d2
phanbadaiphuc222 <72327745+phanbadaiphuc222@users.noreply.github.com>
2023-03-26 10:46:55 +0700

    Update project.sql

--- project.sql
+++ project.sql
@@ -10,15 +10,23 @@ create table users (
 )
 
 insert into users values ('phuc', '0915692401', '05/05/2001', '123', 15000000, 1);
-
 select * from users where username = 'phuc' and pass = '123';
 
-select * from users where username = 'phuc' and pass = '123'
-
-select * from users where phone = '0918090705';
-
-delete from users where pass = '4'
-
-update users set accBalance = 15000000 where phone = '0918090705';
-
 insert into users values ('kiet', '0918090705', '01/01/1963', '123', 15000000, 1)
+select * from users where phone = '0918090705'
+
+create table history (
+	usersname nvarchar(30),
+	phone nvarchar(30),
+	currDate date,
+	act nvarchar(30),
+	sender nvarchar(30),
+	receiver nvarchar(30),
+	amount nvarchar(30)
+)
+
+delete from history
+
+select * from history
+
+update users set accBalance = 15000000
